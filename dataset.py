4cb40a8806cadf335e57ac41390a0632639a235d
Ram Rattan Goyal <codebits.ram@gmail.com>
2021-04-08 23:51:16 +0530

    Second Commit

---
+++ dataset.py
@@ -0,0 +1,125 @@
+#%% Libraries
+import pandas as pd
+import numpy as np
+import random as r
+import matplotlib.pyplot as plt
+from faker import Faker
+from datetime import datetime 
+
+
+
+
+#%% Global Variables
+courseList = ['Python: Introduction','Javascript','Trigonometry','Inverse Trignonometry','Django','Applications Of Trignonometry','HTML,CSS','Node.js','Machine Learning','Data Science','Public Speaking','Literature','Grammar','Political Science']
+
+courseDict={'frontEndList':['HTML,CSS','Javascript','React.js','jQuery','Bootstrap'],'mathsList':
+            ['Trigonometry','Inverse Trignonometry','Applications Of Trignonometry'],
+            'programList':['Python: Introduction','Data Science','Machine Learning','Data Structures','Algorithms'],
+            'backEndList':['Node.js','Django','Ruby on Rails','pHp','Flask'],
+            'politicalList':['Public Speaking','Political Science','Human Rights','Constitution'],
+            'englishList':['Literature','Grammar','Vocabulary','Speech','English Expressions','Poetic Emphasis']
+            }
+    
+typeList = ['frontEndList','mathsList','programList','backendList','politicalList','englishList']
+    
+
+fake = Faker()
+
+#%%
+
+
+def makeSingle(l):
+    li=[]
+    for x in l:
+        for a in x:
+            li.append(a)
+    return li
+
+#%%
+
+df=pd.DataFrame()
+courseColSuper,fieldColSuper = [],[]
+dataDict={}
+
+def retTimeList(n):
+    return np.random.randint(10,30,n+1)
+
+def makeADict(li):
+    for x in li:
+        if x in dataDict.keys():
+            dataDict[x]+=1
+        else:
+            dataDict[x]=1
+
+for i in range(0,1001):
+    r.shuffle(courseList)
+    keyList = list(courseDict.keys())
+    courseCol,fieldCol = [],[]
+    for i in range(0,np.random.randint(1,3)):
+        i3 = np.random.randint(0,len(courseDict.keys()))
+        i1,i2 = np.random.randint(0,len(courseDict[keyList[i3]])),np.random.randint(0,len(courseDict[keyList[i3]]))
+        fieldCol.append(keyList[i3])
+        if i1<i2:
+            courseCol.append(courseDict[keyList[i3]][i1:i2])
+            makeADict(courseDict[keyList[i3]][i1:i2])
+        else:
+            courseCol.append(courseDict[keyList[i3]][i2:i1])
+            makeADict(courseDict[keyList[i3]][i2:i1])     
+            
+    courseColSuper.append(courseCol)
+    fieldColSuper.append(fieldCol)
+    
+temp=[]
+for x in courseColSuper:
+    temp.append(makeSingle(x))
+
+temp2=[]
+for x in fieldColSuper:
+    temp2.append(list(set(x)))
+
+fieldColSuper = temp2
+
+courseColSuper = temp
+df['ID'] = np.arange(1000,2001,1)
+df['Courses'] = np.array(courseColSuper)
+df['TimeSpent'] = df['Courses'].apply(lambda x : retTimeList(len(x)-1))
+df['Field'] = np.array(fieldColSuper)
+
+df.to_csv('courseDetails.csv')
+#%% Top Trending List
+
+top_trending = pd.Series(dataDict).sort_values(ascending=False)[:5]
+
+
+#%% Seek adjustment
+
+
+
+
+#%% Workshop dataframe
+
+
+workshops = pd.DataFrame()
+# workshops['Date']=np.array([datetime.datetime(2021,4,21,12,0,0),datetime.datetime(2021,4,20,2,0,0),datetime.datetime(2021,5,20,2,0,0)])
+workshop_date,workshop_time = [],[]
+selection=[]
+
+for x in range(0,20):
+    date_time = fake.date_time_between(start_date='now',end_date=datetime(2021,6,7,18,0,0))
+    workshop_date.append(date_time.date())
+    workshop_time.append(str(date_time.time().hour)+":00")
+    selection.append(typeList[np.random.randint(0,len(typeList))].split("List")[0].capitalize())
+    
+workshops['Date'] = workshop_date
+workshops['Time'] = workshop_time
+workshops['Duration'] = np.array([str(x)+" hour(s)" for x in np.random.randint(1,4,20)])
+workshops['Field'] = selection
+
+workshops.to_csv('workshops.csv')
+
+
+
+
+
+
+
---
+++ vidToAud.py
@@ -0,0 +1,117 @@
+#%% Library Imports
+
+import subprocess
+import time
+import speech_recognition as sr
+import re
+import string
+import nltk
+import asyncio
+import json
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from fuzzywuzzy import fuzz,process
+
+
+#%% Global Variables
+
+start = time.time()
+r = sr.Recognizer()
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+stop_words.remove("for")
+stop_words.remove("while")
+stop_words.remove("do")
+d={}
+videosContent=[]
+# Command for converting video to audio
+
+
+
+
+#%% Video Processing Functions
+
+
+async def audToText(fileName):
+    
+    with sr.AudioFile("/home/ramji/hk/{}".format(fileName.split(".")[0]+'.wav')) as source:
+        audio_text = r.listen(source,phrase_time_limit=10000,timeout=5)
+        try:
+            text = await r.recognize_google(audio_text).lower()
+            
+        except Exception as e:
+            text = str(e)
+            
+        finally:
+            return text
+            
+
+
+async def vidToAud(fileName,audioname):
+    cmd = 'ffmpeg -y -i "{}" \
+        -ab 160k -ac 2 -ar 44100 -vn "/home/ramji/hk/{}.wav"'.format(fileName,audioname.split('.')[0])
+    subprocess.call(cmd,shell=True)
+    text = await audToText(audioname)
+    print("Text is \n\n\n")
+    return await text
+
+#%% Natural Language Processing
+
+async def theProcessing(name,text):
+    
+    d,vidContent={},{}
+    keywords=[]
+    
+    #Removing all numbers
+    noNum = re.sub(r'\d+','',text)
+    
+    #Removing all punctuations
+    noPunc = noNum.translate(str.maketrans("","", string.punctuation))
+    
+    #Removing stopwords
+    tokens = word_tokenize(noPunc)
+    noStops = [i for i in tokens if not i in stop_words]
+    
+    #Stemming each word
+    noInflections = []
+    for word in noStops:
+        noInflections.append(lemmatizer.lemmatize(word))
+            
+    
+    for word in noInflections:
+        if word in d:
+            d[word]+=1
+        else:
+            d[word]=1
+            
+    max_freq = max(d.values())
+    
+    for word in d.keys():
+        d[word]=d[word]/max_freq
+        if d[word]>0.20: keywords.append(word)
+    
+    vidContent["problem_name"] = name
+    vidContent["keywords"] = keywords
+    
+    return vidContent
+    
+    
+
+#%% The Search
+
+searchResult ={}
+txt = asyncio.ensure_future(vidToAud('pythonIntro.mp4','pythonIntro'))
+# videosContent = asyncio.ensure_future(theProcessing('Ya',txt))
+
+# for content in videosContent:
+#     print(process.extract("layout",content["keywords"],limit=1,scorer=fuzz.token_set_ratio),"\n\n")
+#     print(content["id"])
+
+
+
+
+    
